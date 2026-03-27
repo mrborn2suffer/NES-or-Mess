aafaa214cabc0e08cb904335ee6659f619185f87
mrborn2suffer <mr.born2suffer@gmail.com>
2026-03-27 23:46:20 +0530

    PRG and CHR ROM file extraction logic added

--- Cartridge.java
+++ Cartridge.java
@@ -23,4 +23,60 @@ public class Cartridge
     private int prgBankSelect = 0;
     private int chrBankSelect = 0;
 
+    public Cartridge(String filepath) 
+    {
+        try (FileInputStream fis = new FileInputStream(new File(filepath))) 
+        {
+            byte[] header = new byte[16];
+            fis.read(header);
+            if (header[0] != 'N' || header[1] != 'E' || header[2] != 'S' || header[3] != 0x1A) 
+            {
+                throw new RuntimeException("Invalid NES ROM file!");
+            }
+
+            prgBanks = header[4] & 0xFF;
+            chrBanks = header[5] & 0xFF;
+            
+            boolean verticalMirroring = (header[6] & 0x01) != 0;
+            mirrorMode = verticalMirroring ? 1 : 0; 
+            
+            mapperID = ((header[6] >> 4) & 0x0F) | (header[7] & 0xF0);
+
+            System.out.println("PROGRAM FILE INFO :");
+            System.out.println("Loaded ROM: " + filepath);
+            System.out.println("Mapper ID:  " + mapperID);
+            System.out.println("PRG Size:   " + (prgBanks * 16) + " KB");
+            System.out.println("CHR Size:   " + (chrBanks * 8) + " KB");
+
+            if ((header[6] & 0x04) != 0) 
+            fis.skip(512);
+            prgROM = new byte[prgBanks * 16384];
+            fis.read(prgROM);
+
+            if (chrBanks > 0) 
+            {
+                chrROM = new byte[chrBanks * 8192];
+                fis.read(chrROM);
+            } 
+            else 
+            {
+                chrROM = new byte[8192]; 
+            }
+
+            prgRAM = new byte[8192];
+            if (mapperID == 4) 
+                updateMMC3Pointers();
+            else if (mapperID == 7) 
+            {
+                int maxBanks = prgBanks / 2; 
+                prgBankSelect = maxBanks - 1; 
+            }
+
+        } 
+        catch (IOException e) 
+        {
+            System.err.println("Failed to load ROM: " + e.getMessage());
+        }
+    }
+
 }
