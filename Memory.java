05a14b0c13ce068b99da2db1abb10ac1a25171b5
mrborn2suffer <mr.born2suffer@gmail.com>
2026-04-05 11:41:08 +0530

    Added some Cartridge, PPU and CPU methods

--- Memory.java
+++ Memory.java
@@ -13,4 +13,52 @@ public class Memory
         this.controller = new Controller();
     }
     
+    public void loadCartridge(Cartridge cartridge) 
+    { 
+        this.cartridge = cartridge; 
+    }
+    public Cartridge getCartridge() 
+    { 
+        return cartridge; 
+    }
+    public void setPPU(PPU ppu) 
+    { 
+        this.ppu = ppu; 
+    }
+    public Controller getController() 
+    { 
+        return controller; 
+    }
+    public void setCPU(CPU cpu) 
+    { 
+        this.cpu = cpu; 
+    }
+    
+    public byte read(int address) 
+    {
+        address &= 0xFFFF; 
+        
+        if (address < 0x2000) 
+        {
+            return ram[address & 0x7FF];
+        } 
+        else if (address >= 0x8000) 
+        {
+            return (byte)(cartridge != null ? cartridge.readPRG(address - 0x8000) : 0);
+        } 
+        else if (address >= 0x6000 && address < 0x8000) 
+        {
+            return (byte)(cartridge != null ? cartridge.readPRGRAM(address - 0x6000) : 0);
+        } 
+        else if (address >= 0x2000 && address < 0x4000) 
+        {
+            return readPPURegister(0x2000 + (address & 0x7));
+        } 
+        else if (address >= 0x4000 && address < 0x4020) 
+        {
+            return readIORegister(address);
+        }
+        return 0;
+    }
+
 }
