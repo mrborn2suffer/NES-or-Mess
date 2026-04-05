894e945cab5303683ad837ab44a2ea47d08b0a42
mrborn2suffer <mr.born2suffer@gmail.com>
2026-04-05 23:08:47 +0530

    Implemented remaining memory methods

--- Memory.java
+++ Memory.java
@@ -61,4 +61,78 @@ public class Memory
         return 0;
     }
 
+    public void write(int address, byte value) 
+    {
+        address &= 0xFFFF; 
+        
+        if (address < 0x2000) 
+        {
+            ram[address & 0x7FF] = value;
+        } 
+        else if (address >= 0x8000) 
+        {
+            if (cartridge != null) cartridge.writePRG(address - 0x8000, value);
+        } 
+        else if (address >= 0x6000 && address < 0x8000) 
+        {
+            if (cartridge != null) cartridge.writePRGRAM(address - 0x6000, value);
+        } 
+        else if (address >= 0x2000 && address < 0x4000) 
+        {
+            writePPURegister(0x2000 + (address & 0x7), value);
+        } 
+        else if (address >= 0x4000 && address < 0x4020) 
+        {
+            writeIORegister(address, value);
+        }
+    }
+    
+    private byte readPPURegister(int address) 
+    {
+        return (byte)(ppu != null ? ppu.readRegister(address) : 0);
+    }
+    
+    private void writePPURegister(int address, byte value) 
+    {
+        if (ppu != null) 
+        ppu.writeRegister(address, value & 0xFF);
+    }
+    
+    private byte readIORegister(int address) 
+    {
+        if (address == 0x4016) 
+        {
+            return (byte)(controller != null ? controller.read() : 0);
+        }
+        return 0;
+    }
+    
+    private void writeIORegister(int address, byte value) 
+    {
+        if (address == 0x4014) 
+        {
+            writePPURegister(0x2003, (byte)0x00); 
+            int page = (value & 0xFF) << 8;
+            for (int i = 0; i < 256; i++) 
+            {
+                writePPURegister(0x2004, read(page + i));
+            }
+        } 
+        else if (address == 0x4016) 
+        {
+            if (controller != null) 
+            controller.write(value);
+        }
+    }
+    
+    public short readWord(int address) 
+    {
+        return (short)((read(address) & 0xFF) | ((read(address + 1) & 0xFF) << 8));
+    }
+    
+    public void writeWord(int address, short value) 
+    {
+        write(address, (byte)(value & 0xFF));   
+        write(address + 1, (byte)((value >> 8) & 0xFF));
+    }
 }
