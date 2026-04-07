f86eef0a1aea093c5b4994dcc4d7bbab061cd026
mrborn2suffer <mr.born2suffer@gmail.com>
2026-04-07 18:51:33 +0530

    reset() and increment() added

--- PPU.java
+++ PPU.java
@@ -22,5 +22,43 @@ public class PPU
         this.cpu = cpu; 
     }
     
+     public void reset() 
+    {
+        control = mask = status = oamAddr = oamData = scroll = addr = data = 0;
+        vramAddr = tempVramAddr = fineX = scanline = cycle = frame = 0;
+        writeToggle = false;
+        for (int i = 0; i < oam.length; i++) 
+            oam[i] = 0;
+        for (int i = 0; i < vram.length; i++) 
+            vram[i] = 0;
+        for (int i = 0; i < palette.length; i++) 
+            palette[i] = 0;
+    }
     
+    private void incrementY() 
+    {
+        if ((vramAddr & 0x7000) != 0x7000) 
+        {
+            vramAddr += 0x1000;
+        } 
+        else 
+        {
+            vramAddr &= ~0x7000;
+            int y = (vramAddr & 0x03E0) >> 5;
+            if (y == 29) 
+            {
+                y = 0;
+                vramAddr ^= 0x0800;
+            } 
+            else if (y == 31) 
+            {
+                y = 0;
+            } 
+            else 
+            {
+                y++;
+            }
+            vramAddr = (vramAddr & ~0x03E0) | (y << 5);
+        }
+    }
 }
