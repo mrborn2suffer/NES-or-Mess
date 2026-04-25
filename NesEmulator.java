b7a0500f9531d36ed0cf491cc56777600299449e
mrborn2suffer <mr.born2suffer@gmail.com>
2026-04-25 13:27:23 +0530

    Loading cartridge, PPU, CPU and Memory for the uploaded file...

---
+++ NesEmulator.java
@@ -0,0 +1,54 @@
+public class NesEmulator 
+{
+    private Memory memory;
+    private CPU cpu;
+    private PPU ppu;
+
+    public NesEmulator() 
+    {
+        memory = new Memory();
+        cpu = new CPU(memory);
+        ppu = new PPU(memory);
+
+        memory.setPPU(ppu);
+        ppu.setCPU(cpu);
+        memory.setCPU(cpu);
+    }
+
+    public void loadCartridge(String filepath) 
+    {
+        Cartridge cart = new Cartridge(filepath);
+        memory.loadCartridge(cart);
+        cpu.reset();
+        ppu.reset();
+        
+        if (memory.getController() != null) 
+        {
+            memory.getController().clear();
+        }
+    }
+
+    public void step() 
+    {
+        int cyclesBefore = cpu.getCycles();
+        cpu.step();
+        int cyclesTaken = cpu.getCycles() - cyclesBefore;
+        for (int i = 0; i < cyclesTaken * 3; i++) 
+        {
+            ppu.step();
+        }
+    }
+
+    public CPU getCPU() 
+    { 
+        return cpu; 
+    }
+    public PPU getPPU() 
+    { 
+        return ppu; 
+    }
+    public Memory getMemory() 
+    { 
+        return memory; 
+    }
+}
