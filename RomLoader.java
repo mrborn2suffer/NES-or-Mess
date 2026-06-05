cd315e40708d58c45423052c64ae30849293371e
mrborn2suffer <mr.born2suffer@gmail.com>
2026-06-05 11:12:05 +0530

    Apply minimalistic B&W theme and Pricedown font to RomLoader list

--- RomLoader.java
+++ RomLoader.java
@@ -4,10 +4,18 @@ import java.io.File;
 
 public class RomLoader extends JFrame 
 {
+    private static Font pricedownFont = null;
+    static {
+        try {
+            pricedownFont = Font.createFont(Font.TRUETYPE_FONT, new File("pricedown.otf"));
+        } catch (Exception e) {
+            pricedownFont = new Font("SansSerif", Font.BOLD, 22);
+        }
+    }
+
     private java.util.List<File> romFiles = new java.util.ArrayList<>();
     private JPanel contentPanel;
     private JList<String> listComponent;
-    private JPanel gridComponent;
 
     public RomLoader() 
     {
@@ -16,17 +24,19 @@ public class RomLoader extends JFrame
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(960, 720);
         setLocationRelativeTo(null);
+        getContentPane().setBackground(Color.BLACK);
         
         contentPanel = new JPanel(new CardLayout());
+        contentPanel.setBackground(Color.BLACK);
         
         DefaultListModel<String> listModel = new DefaultListModel<>();
         for (File f : romFiles) listModel.addElement(f.getName().toUpperCase().replace(".NES", ""));
         listComponent = new JList<>(listModel);
+        listComponent.setBackground(Color.BLACK);
+        listComponent.setForeground(Color.WHITE);
+        listComponent.setFont(pricedownFont.deriveFont(22.0f));
+        
         contentPanel.add(new JScrollPane(listComponent), "LIST");
-
-        gridComponent = new JPanel(new GridLayout(5, 5, 10, 10));
-        contentPanel.add(gridComponent, "GRID");
-
         add(contentPanel, BorderLayout.CENTER);
     }
 
